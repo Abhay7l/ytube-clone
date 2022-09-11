6e290e3d1f809f7ea088e037744e3fa99df48da0
Abhay7l <abhaygupta24803@gmail.com>
2022-09-12 00:52:47 +0530

    updating our directory page with directory component and category item component

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+// import './App.css';
+import Directory from './components/Directory/directory.component';
+
+function App() {
+
+const categories=[
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
+  return (
+   <Directory categories={categories} className="directory-container" />//this will be the container for our components
+  );
+}
+
+export default App;
