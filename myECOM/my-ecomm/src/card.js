46230d5a282294f6e36052935bf9c57434052fb7
Abhay7l <abhaygupta24803@gmail.com>
2022-11-09 00:46:00 +0530

    updating our directory page

---
+++ myECOM/my-ecomm/src/card.js
@@ -0,0 +1,7 @@
+
+// const card=(props)=>{
+//     return(
+//         <div className="category-container"><div className="category-body-container"><h1>{this.props}</h1><p>HATS</p></div></div>
+//     )
+// }
+// export default card;
